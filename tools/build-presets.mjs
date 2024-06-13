79ad294799c0f405136d9cb11902e22822fc8835
Matthew Wilmsmeyer <mwilmsm@gmail.com>
2024-06-12 19:32:37 -0500

    ES-Module - Got hygen working finally. -mw

--- tools/build-presets.mjs
+++ tools/build-presets.mjs
@@ -3,22 +3,26 @@
 // Usage: tools/build-presets [preset-name]
 
 import { runner } from 'hygen';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import presets from '../src/presets.mjs';
+
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
 
 process.env.HYGEN_OVERWRITE = 1
 Error.stackTraceLimit = Infinity;
 
-// presets.forEach((name) => {
-  const name = "adventure";
+presets.forEach((name) => {
   console.log("name", name);
   runner(['preset', 'new', name], {
-    templates: '../templates',
+    templates: path.join(__dirname, '../templates'),
     cwd: process.cwd(),
     logger: {
       ok: console.log.bind(console),
       log: console.log.bind(console),
     },
-    createPrompter: function() {},
+    createPrompter: function () { },
     debug: true,
   });
-// });
+});
